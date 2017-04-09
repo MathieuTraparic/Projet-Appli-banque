acb8b4712ffd3084b47d709aee3eb27946b6ceba
LucienBouletRoblin <lucien.bouletroblin@gmail.com>
2017-04-09 18:56:45 +0200

    previous button event of create user

--- BankEclipse/src/CreateUserController.java
+++ BankEclipse/src/CreateUserController.java
@@ -7,7 +7,16 @@ public class CreateUserController {
 		VistaNavigator.loadVista(VistaNavigator.CREATE_USER_2);
 	}
 	@FXML
+	void previousButton(ActionEvent event){
+		VistaNavigator.loadVista(VistaNavigator.CREATE_USER_1);
+	}
+	@FXML
 	void cancelButton(ActionEvent event){
 		VistaNavigator.loadVista(VistaNavigator.LOGIN);
+	}
+	
+	@FXML
+	void signInButton(ActionEvent event){
+		VistaNavigator.loadVista(VistaNavigator.TEMPLATE);
 	}
 }
