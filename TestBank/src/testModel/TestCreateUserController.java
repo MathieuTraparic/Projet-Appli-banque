d67802c35efb611161d5056c847962c218098537
LucienBouletRoblin <lucien.bouletroblin@gmail.com>
2017-04-11 15:33:51 +0200

    TestOwner

---
+++ TestBank/src/testModel/TestCreateUserController.java
@@ -0,0 +1,14 @@
+package testModel;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestCreateUserController {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
