ba4d400b6b0fe941c168140cfe62e18f1f5a028a
DavidRiviere <david_riviere@hotmail.fr>
2017-04-11 08:45:16 +0200

    Modification de Account.java et implémentation de la test correspondante

--- BankEclipse/src/model/Account.java
+++ BankEclipse/src/model/Account.java
@@ -2,11 +2,42 @@ package model;
 
 public class Account {
 	private String number;
-	private String descrition;
-	private double initialBalance;
+	private String description;
+	private Double initialBalance;
 	private int overdraft;
-	private double alertTreshold;
+	private Double alertThreshold;
 	
+	public Account(String number, String description,Double initialBalance, int overdraft, Double threshold){
+		if(number.equals(null)){
+			throw new NullPointerException ("The account number can't be null");
+		}
+		if(description.equals(null)){
+			throw new NullPointerException ("The account description can't be null");
+		}
+		if(initialBalance == null){
+			throw new NullPointerException ("The initial balance can't be null");
+		}
+		if(threshold == null){
+			throw new NullPointerException ("The initial balance can't be null");
+		}
+		
+		this.number = number;
+		this.initialBalance = initialBalance;
+		this.overdraft = overdraft;
+		this.alertThreshold = threshold;
+	}
 	
+	public String getAccountNumber(){
+		if(this.number.length()==0){
+			throw new IllegalArgumentException("Account number can't be empty");
+		}
+		return this.number;
+	}
 	
+	public String getAccountDescription(){
+		if(this.description.length()==0){
+			throw new IllegalArgumentException("Account number can't be empty");
+		}
+		return this.description;
+	}
 }
