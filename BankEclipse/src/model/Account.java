466928553572b3aa7556fea28fe2ea111ef69bce
MathieuTraparic <mathieu.traparic@gmail.com>
2017-04-11 15:44:22 +0200

    account and his test, name validator

--- BankEclipse/src/model/Account.java
+++ BankEclipse/src/model/Account.java
@@ -2,11 +2,12 @@ package model;
 
 import java.util.ArrayList;
 
+
 public class Account {
 	private String number;
 	private String description;
-	private Double initialBalance;
-	private int overdraft;
+	private double initialBalance;
+	private double overdraft;
 	private Double alertThreshold;	
 	private String countryCode;
 	private String type;
@@ -15,7 +16,7 @@ public class Account {
 	 * @author user
 	 *To be fetched from DB before constructor
 	 */
-	private static ArrayList<String> COUNTRYCODE = new ArrayList<String>(){
+	private static ArrayList<String> COUNTRYCODES = new ArrayList<String>(){
 		{
 			add("FR");
 			add("CH");
@@ -26,27 +27,21 @@ public class Account {
 	 * @author user
 	 *To be fetched from DB before constructor
 	 */
-	private static ArrayList<String> TYPE = new ArrayList<String>(){
+	private static ArrayList<String> TYPES = new ArrayList<String>(){
 		{
 			add("EPARGNE");
 			add("COMPTE_COURANT");
 		}
 	};
 	
-	public Account(String number, String description,Double initialBalance,
-			int overdraft, Double threshold, String countryCode, String type){
-		if(number.isEmpty()){
-			throw new IllegalArgumentException("The account number can't be empty");
-		}
-		if(description.isEmpty()){
-			throw new IllegalArgumentException ("The account description can't be empty");
-		}
-		if (countryCode.isEmpty()){
-			throw new IllegalArgumentException ("The country code can't be empty");
-		}
-		if (type.isEmpty()){
-			throw new IllegalArgumentException ("The account type can't be empty");
-		}
+	public Account(String number, String description,double initialBalance,
+			 double overdraft, Double threshold, String countryCode, String type){
+		check_number(number);
+		check_description(description);
+		check_countryCode(countryCode);
+		check_type(type);
+		
+		
 		
 		this.number = number;
 		this.initialBalance = initialBalance;
@@ -71,15 +66,51 @@ public class Account {
 		return this.description;
 	}
 	
-	public Double getInitialBalance(){
+	public double getInitialBalance(){
 		return this.initialBalance;
 	}
 	
-	public int getOverdraft(){
+	public double getOverdraft(){
 		return this.overdraft;
 	}
 	
-	public Double getAlertThreshold(){
-		return this.alertThreshold;
+	public double getAlertThreshold(){
+		return this.alertThreshold.doubleValue();
 	}
+	
+	public static void check_number(String number) throws IllegalArgumentException {
+		if(number.isEmpty()){
+			throw new IllegalArgumentException("The account number cannot be empty");
+		}
+	}
+	
+	public static void check_description(String description) throws IllegalArgumentException {
+		if(description.isEmpty()){
+			throw new IllegalArgumentException ("The account description can't be empty");
+		}
+	}
+	
+	public static void check_countryCode(String countryCode) throws IllegalArgumentException {
+		if (countryCode.isEmpty()){
+			throw new IllegalArgumentException ("The country code can't be empty");
+		}else if(!COUNTRYCODES.contains(countryCode)){
+			throw new IllegalArgumentException ("The country code must be an existing country code");
+		}
+	}
+	
+	public static void check_type(String type) throws IllegalArgumentException {
+		if (type.isEmpty()){
+			throw new IllegalArgumentException ("The account type can't be empty");
+		}else if(!TYPES.contains(type)){
+			throw new IllegalArgumentException ("The account type must be an existing account type");
+		}
+	}
+	
+	public static void check_overdraft(double overdraft) throws IllegalArgumentException {
+		if(overdraft>0){
+			throw new IllegalArgumentException ("Authorised overdraft cannot be positive");
+		}
+	}
+	
+	
 }
--- TestBank/src/testModel/TestAccount.java
+++ TestBank/src/testModel/TestAccount.java
@@ -7,36 +7,30 @@ import org.junit.Test;
 import model.Account;
 
 public class TestAccount {
+	private Account account;
+	
 	// Account Number
 	@Test(expected = NullPointerException.class)
 	public void test_NullAccountNumber() {
-		Account account = new Account(null, "Comment",1000d, 150, 0d, null, null);
+		account = new Account(null, "Comment",1000d, 150, 0d, "FR", "EPARGNE");
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
 	public void test_getAccountNumberEmpty(){
-		Account account = new Account("", "Comment",1000d, 150, 0d, null, null);
+		account = new Account("", "Comment",1000d, 150, 0d,"FR", "EPARGNE");
 	}
 	
 	// Account description
 	@Test(expected = NullPointerException.class)
 	public void test_NullDescription() {
-		Account account = new Account("NA", null,1000d, 150, 0d, null, null);
+		account = new Account("NA", null,1000d, 150, 0d,"FR", "EPARGNE");
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
 	public void test_getAccountDescriptionEmpty(){
-		Account account = new Account("NA", "",1000d, 150, 0d, null, null);
+		account = new Account("NA", "",1000d, 150, 0d,"FR", "EPARGNE");
 	}
 	
-	// Initial Balance
-	@Test(expected = NullPointerException.class)
-	public void test_NullInitialBalance() {
-		Account account = new Account("NA", "Comment",null, 150, 0d, null, null);
-	}
+
 	
-	@Test(expected = NullPointerException.class)
-	public void test_NullAlertThreshold() {
-		Account account = new Account("NA", "Comment",1000d, 150, null, null, null);
-	}
 }
