ff7a563f38de3e74593c578cd9b22ea875561acc
LucienBouletRoblin <lucien.bouletroblin@gmail.com>
2017-04-12 13:00:56 +0200

    advisor controller but problem with date not fixed yet

--- BankEclipse/src/controllers/AdvisorController.java
+++ BankEclipse/src/controllers/AdvisorController.java
@@ -1,15 +1,25 @@
 package controllers;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Date;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
+import model.Account;
+import model.Advisor;
 
 public class AdvisorController {
 
 	@FXML
 	void applyAdvisorChange(ActionEvent event){
-		
+		Advisor advisor = new Advisor(name.getText(), 
+				firstName.getText(),
+				phoneNumber.getText(),
+				email.getText(),
+				Date.from(assignmentDate.getValue().toInstant()));
 	}
 	
 	@FXML
@@ -17,20 +27,11 @@ public class AdvisorController {
 		//TODO choose the bank link to an account and advisor
 	}
 	
-	
-	@FXML
-	private TextField name;
-	
-	@FXML
-	private TextField firstName;
-	
-	@FXML
-	private TextField phoneNumber;
-	
-	@FXML
-	private TextField email;
-	
-	@FXML
-	private DatePicker assignmentDate;
+
+	public TextField name;
+	public TextField firstName;
+	public TextField phoneNumber;
+	public TextField email;
+	public DatePicker assignmentDate;
 	
 }
