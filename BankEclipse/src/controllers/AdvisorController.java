4ffe297574fcedc00ab44a12a49e5ec79d821d23
LucienBouletRoblin <lucien.bouletroblin@gmail.com>
2017-04-12 16:56:23 +0200

    advisor

--- BankEclipse/src/controllers/AdvisorController.java
+++ BankEclipse/src/controllers/AdvisorController.java
@@ -6,6 +6,7 @@ import java.util.GregorianCalendar;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.Advisor;
 
@@ -15,29 +16,39 @@ public class AdvisorController {
 	public TextField firstName;
 	public TextField phoneNumber;
 	public TextField email;
-	public DatePicker assignmentDate;	
+	public DatePicker assignmentDate;
+	public Label invalidName;
+	public Label invalidFirstName;
+	public Label invalidNumber;
+	public Label invalidEmail;
 	
 	@FXML
 	void applyAdvisorChange(ActionEvent event) {
 		
-		String err="";
-		
-		if (!Advisor.isValidName(name.getText())){
-			err+="Name is incorrect, must contains only letters and/or spaces, dashes, apostrophe";
-		}
-		else if (!Advisor.isValidFirstName(firstName.getText())){
-			
-		}
-		
 		Calendar cal = new GregorianCalendar(assignmentDate.getValue().getYear(),
 				assignmentDate.getValue().getMonthValue(), 
 				assignmentDate.getValue().getDayOfMonth(), 0, 0);
-
-				
+		
+		if (!Advisor.isValidName(name.getText())){
+			invalidName.setText("Name is incorrect, must contains only letters and/or spaces, dashes, apostrophe");
+		}
+		else if (!Advisor.isValidFirstName(firstName.getText())){
+			invalidFirstName.setText("First name is incorrect, must contains only letters and/or spaces, dashes, apostrophe");
+		}
+		else if (!Advisor.isValidPhoneNumber(phoneNumber.getText())){
+			invalidNumber.setText("Phone number must be a valid French phone number composed of 10 digits");
+		}
+		else if (!Advisor.isValidEmail(email.getText())){
+			invalidEmail.setText("Email must be a valid email address");
+		}
+		else if (!Advisor.isValidAssignmentDate(cal.getTime())){
+			invalidEmail.setText("Email must be a valid email address");
+		}
+		else{		
 		Advisor advisor = new Advisor(name.getText(), 
 						firstName.getText(), phoneNumber.getText(), email.getText(),
 						cal.getTime());
-
+		}
 	}
 
 	@FXML
