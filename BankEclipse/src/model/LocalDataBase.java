d9d27f25a0c943f9aeed658812db6f7e134aee69
MathieuTraparic <mathieu.traparic@gmail.com>
2017-04-13 11:29:56 +0200

    gitignore update

--- BankEclipse/src/model/LocalDataBase.java
+++ BankEclipse/src/model/LocalDataBase.java
@@ -22,6 +22,30 @@ public class LocalDataBase implements Observable{
 	HashMap<String, String> transactionType;
 	
 	
+	public HashMap<String, String> getFrequencies() {
+		return this.frequencies;
+	}
+
+	public void setFrequencies(HashMap<String, String> frequencies) {
+		this.frequencies = frequencies;
+	}
+
+	public HashMap<String, String> getCountryCodes() {
+		return this.countryCodes;
+	}
+
+	public void setCountryCodes(HashMap<String, String> countryCodes) {
+		this.countryCodes = countryCodes;
+	}
+
+	public HashMap<String, String> getTransactionType() {
+		return this.transactionType;
+	}
+
+	public void setTransactionType(HashMap<String, String> transactionType) {
+		this.transactionType = transactionType;
+	}
+
 	private LocalDataBase(){
 		frequencies = new HashMap<String, String>();
 		countryCodes =  new HashMap<String, String>();
