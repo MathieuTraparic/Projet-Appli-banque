cb35094f2d18d970f1c3ec87d0941c510d25388a
LucienBouletRoblin <lucien.bouletroblin@gmail.com>
2017-04-13 16:31:07 +0200

    advisor controller is working, hopefully...

--- BankEclipse/src/controllers/AdvisorController.java
+++ BankEclipse/src/controllers/AdvisorController.java
@@ -10,6 +10,7 @@ import com.sun.corba.se.impl.javax.rmi.CORBA.Util;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.Label;
@@ -21,63 +22,89 @@ import model.Bank;
 import util.Utils;
 
 public class AdvisorController implements Initializable{
-	
-	public TextField name;
-	public TextField firstName;
-	public TextField phoneNumber;
-	public TextField email;
-	public DatePicker assignmentDate;
-	public Label invalidName;
-	public Label invalidFirstName;
-	public Label invalidNumber;
-	public Label invalidEmail;
-	public Label invalidDate;
-	public ComboBox<String> agency;
-	public ComboBox<String> bank;
-	
+
+	@FXML public TextField name;
+	@FXML public TextField firstName;
+	@FXML public TextField phoneNumber;
+	@FXML public TextField email;
+	@FXML public DatePicker assignmentDate;
+	@FXML public Label invalidName;
+	@FXML public Label invalidFirstName;
+	@FXML public Label invalidNumber;
+	@FXML public Label invalidEmail;
+	@FXML public Label invalidDate;
+	@FXML public Label invalidAgency;
+	@FXML public Label invalidBank;
+	@FXML public ComboBox<String> agency;
+	@FXML public ComboBox<String> bank;
+	@FXML public Button applyButton;
+
+
 	@FXML
 	void applyAdvisorChange(ActionEvent event) {
-		
-		Utils.settingLabelsInvisible(invalidDate,invalidEmail,invalidFirstName,invalidName,invalidNumber);
-		
+
+		Utils.settingLabelsInvisible(invalidAgency,invalidDate,invalidEmail,invalidFirstName,invalidName,invalidNumber);
+
 		Calendar cal;
-		
-		if (assignmentDate!=null ){
-			cal = new GregorianCalendar(assignmentDate.getValue().getYear(),
-					assignmentDate.getValue().getMonthValue(), 
-					assignmentDate.getValue().getDayOfMonth(), 0, 0, 0);
-			if (!Advisor.isValidAssignmentDate(cal.getTime())){
+
+		if (bank.getValue()==null){
+			applyButton.setDisable(true);
+			assignmentDate.setDisable(true);
+			name.setDisable(true);
+			firstName.setDisable(true);
+			phoneNumber.setDisable(true);
+			email.setDisable(true);
+			agency.setDisable(true);
+			
+			invalidBank.setVisible(true);
+		}
+		else{
+
+			if (agency.getValue()==null){
+				invalidAgency.setVisible(true);
+			}
+
+			if (assignmentDate.getValue()==null ){
 				invalidDate.setVisible(true);
 			}
-		}
-		else {
-			invalidDate.setVisible(true);
+			else {
+				cal = new GregorianCalendar(assignmentDate.getValue().getYear(),
+						assignmentDate.getValue().getMonthValue(),
+						assignmentDate.getValue().getDayOfMonth(), 
+						0, 0, 0);
+				if (!Advisor.isValidAssignmentDate(cal.getTime())){
+					invalidDate.setVisible(true);
+				}
+			}
 
+			if (name.getText().isEmpty()||!Advisor.isValidName(name.getText())){
+				invalidName.setVisible(true);
+			}
+			if (firstName.getText().isEmpty()||!Advisor.isValidFirstName(firstName.getText())){
+				invalidFirstName.setVisible(true);
+			}
+			if (phoneNumber.getText().isEmpty()||!Advisor.isValidPhoneNumber(phoneNumber.getText())){
+				invalidNumber.setVisible(true);
+			}
+			if (email.getText().isEmpty()||!Advisor.isValidEmail(email.getText())){
+				invalidEmail.setVisible(true);			
+			}
+
+			if (!invalidEmail.isVisible()&& !invalidDate.isVisible()
+					&&!invalidFirstName.isVisible()&&!invalidName.isVisible()
+					&&!invalidNumber.isVisible()&& !invalidAgency.isVisible()
+					&&!invalidBank.isVisible()){		
+				cal = new GregorianCalendar(assignmentDate.getValue().getYear(),
+						assignmentDate.getValue().getMonthValue(), 
+						assignmentDate.getValue().getDayOfMonth(), 0, 0, 0);
+				Advisor advisor = new Advisor(name.getText(), 
+						firstName.getText(), phoneNumber.getText(), email.getText(),
+						cal.getTime());
+			}
 		}
-		if (!Advisor.isValidName(name.getText())){
-			invalidName.setVisible(true);
-		}
-		if (!Advisor.isValidFirstName(firstName.getText())){
-			invalidFirstName.setVisible(true);
-		}
-		if (!Advisor.isValidPhoneNumber(phoneNumber.getText())){
-			invalidNumber.setVisible(true);
-		}
-		if (!Advisor.isValidEmail(email.getText())){
-			invalidEmail.setVisible(true);
-		}
-		
-		else{		
-			cal = new GregorianCalendar(assignmentDate.getValue().getYear(),
-					assignmentDate.getValue().getMonthValue(), 
-					assignmentDate.getValue().getDayOfMonth(), 0, 0, 0);
-			Advisor advisor = new Advisor(name.getText(), 
-							firstName.getText(), phoneNumber.getText(), email.getText(),
-							cal.getTime());
-		}
-	
+
 	}
-	
+
 	@Override
 	public void initialize(URL fxmlFileLocation, ResourceBundle resources) {
 		for (String l : Agency.getAgency()) {
@@ -94,6 +121,24 @@ public class AdvisorController implements Initializable{
 
 	@FXML
 	void chooseAdvisorBank(ActionEvent event) {
+		if (bank.getValue()==null){
+			applyButton.setDisable(true);
+			agency.setDisable(true);
+			assignmentDate.setDisable(true);
+			name.setDisable(true);
+			firstName.setDisable(true);
+			phoneNumber.setDisable(true);
+			email.setDisable(true);
+		}
+		else{
+			applyButton.setDisable(false);
+			agency.setDisable(false);
+			assignmentDate.setDisable(false);
+			name.setDisable(false);
+			firstName.setDisable(false);
+			phoneNumber.setDisable(false);
+			email.setDisable(false);
+		}
 		// TODO choose the bank link to an account and advisor
 	}
 
