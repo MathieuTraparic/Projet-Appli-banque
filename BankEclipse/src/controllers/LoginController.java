a49df5c3341af4f4d398177846a77268496dfaa0
MathieuTraparic <mathieu.traparic@gmail.com>
2017-04-13 15:55:03 +0200

    added fields in loginController

--- BankEclipse/src/controllers/LoginController.java
+++ BankEclipse/src/controllers/LoginController.java
@@ -3,12 +3,18 @@ package controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class LoginController {
-
+	
+	@FXML TextField login;
+	@FXML PasswordField pswd;
+	
 	@FXML
 	void signInButton(ActionEvent event){
-		VistaNavigator.loadVista(VistaNavigator.TEMPLATE);
+		
+		//VistaNavigator.loadVista(VistaNavigator.TEMPLATE);
 	}
 	
 	@FXML
