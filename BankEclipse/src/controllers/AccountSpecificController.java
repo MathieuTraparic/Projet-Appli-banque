79b59e4ea346ee2a8c870fcb46f094e944caf7b7
MathieuTraparic <mathieu.traparic@gmail.com>
2017-04-21 11:46:03 +0200

    comments and a slight fix

--- BankEclipse/src/controllers/AccountSpecificController.java
+++ BankEclipse/src/controllers/AccountSpecificController.java
@@ -38,32 +38,38 @@ public abstract class AccountSpecificController implements Initializable {
 	public void initialize(URL location, ResourceBundle resources) {
 		this.accountCombo.setDisable(true);
 
-		Owner o = VistaNavigator.getInstance().getLoggedOwner();
+		Owner loggedOwner = VistaNavigator.getInstance().getLoggedOwner();
 
 		EntityManager em = VistaNavigator.getEmf().createEntityManager();
 
 		// get the accounts from logged owner
 		this.accounts = new HashSet<>();
 		TypedQuery<List> q = em.createQuery("SELECT o.accounts FROM Owner o WHERE o=:loggedOwner", List.class);
-		accounts.addAll((Collection<? extends Account>) q.setParameter("loggedOwner", o).getResultList());
-
+		accounts.addAll((Collection<? extends Account>) q.setParameter("loggedOwner", loggedOwner).getResultList());
+		
+		//get a set of agencies from the accounts
 		HashSet<Agency> agencies = new HashSet<>();
 		accounts.forEach(account -> agencies.add(account.getAgency()));
-
+		
+		//get a set of banks from the agencies
 		this.banks = new HashSet<>();
 		agencies.forEach(agency -> banks.add(agency.getBank()));
 
 		this.bankCombo.getItems().addAll(banks);
 
 		this.bankCombo.valueProperty().addListener((observable, oldValue, newValue) -> {
+			//get a bank specific subset of all the account from the owner 
 			HashSet<Account> accountFromCurrentBank = new HashSet<>();
 			this.accounts.forEach(account-> {
 				if(account.getAgency().getBank().equals(bankCombo.getValue())){
 					accountFromCurrentBank.add(account);
 				}
 			});
+			
+			//update the account comboBox and reset its current value
 			this.accountCombo.setDisable(false);
 			this.accountCombo.getItems().setAll(accountFromCurrentBank);
+			this.accountCombo.setValue(null);
 		});
 
 	}
