b1aab23aa0d04fe8b52a3b433a53fcd7ce90090a
MathieuTraparic <mathieu.traparic@gmail.com>
2017-04-25 09:48:52 +0200

    added bankCombo handling in homeController

--- BankEclipse/src/controllers/HomeController.java
+++ BankEclipse/src/controllers/HomeController.java
@@ -13,58 +13,57 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ComboBox;
 import javafx.stage.WindowEvent;
+import model.Account;
 import model.Address;
 import model.Agency;
 import model.Bank;
 import model.CpVille;
 import util.PopWindow;
+import javafx.scene.control.TableView;
 
-public class HomeController implements Initializable{
-	
-	@FXML public ComboBox<String> bankChoiceHome;
-	
+public class HomeController extends BankSelector {
+
+
 	private Bank b = null;
+	@FXML TableView<Account> accountView;
 
 	@FXML
 	void handleAddBankHome(ActionEvent event) throws IOException {
-		PopupController<Bank> controller = PopupController.load(
-				VistaNavigator.ADD_BANK,false);
-		controller.show(new Bank("name","code"),
-			new EventHandler<WindowEvent>(){
-				@Override
-				public void handle(WindowEvent event){
-					b = controller.getValidatedData();
-					if (b!=null){
-						EntityManager em = VistaNavigator.getEmf().createEntityManager();
-						em.getTransaction().begin();
-						em.persist(b);
-						em.getTransaction().commit();
-						em.close();
-					}
-			}	
+		PopupController<Bank> controller = PopupController.load(VistaNavigator.ADD_BANK, false);
+		controller.show(new Bank("name", "code"), new EventHandler<WindowEvent>() {
+			@Override
+			public void handle(WindowEvent event) {
+				b = controller.getValidatedData();
+				if (b != null) {
+					EntityManager em = VistaNavigator.getEmf().createEntityManager();
+					em.getTransaction().begin();
+					em.persist(b);
+					em.getTransaction().commit();
+					em.close();
+				}
+			}
 		});
 	}
 
 	@FXML
 	void handleAddAgencyHome(ActionEvent event) throws IOException {
-		PopupController<Agency> controller = PopupController.load(
-				VistaNavigator.ADD_AGENCY,false);
-		controller.show(new Agency("name","counterCode"),
-			new EventHandler<WindowEvent>(){
-				@Override
-				public void handle(WindowEvent event){
-					Agency a = controller.getValidatedData();
+		PopupController<Agency> controller = PopupController.load(VistaNavigator.ADD_AGENCY, false);
+		controller.show(new Agency("name", "counterCode"), new EventHandler<WindowEvent>() {
+			@Override
+			public void handle(WindowEvent event) {
+				Agency a = controller.getValidatedData();
 
-					//Actually I don't get the idea why we do this here and not when we submit in the popup window
-					if (a!=null){
-						EntityManager em = VistaNavigator.getEmf().createEntityManager();
-	
-						em.getTransaction().begin();
-						em.persist(a);
-						em.getTransaction().commit();
-						em.close();
-					}
-			}	
+				// Actually I don't get the idea why we do this here and not
+				// when we submit in the popup window
+				if (a != null) {
+					EntityManager em = VistaNavigator.getEmf().createEntityManager();
+
+					em.getTransaction().begin();
+					em.persist(a);
+					em.getTransaction().commit();
+					em.close();
+				}
+			}
 		});
 	}
 
@@ -75,16 +74,11 @@ public class HomeController implements Initializable{
 
 	@FXML
 	void handleBankChoiceHome(ActionEvent event) {
-		
+
 	}
-	@SuppressWarnings("unchecked")
+
 	@Override
 	public void initialize(URL fxmlFileLocation, ResourceBundle resources) {
-		EntityManager em = VistaNavigator.getEmf().createEntityManager();
-		List<Bank> l = em.createNamedQuery("Bank.findAll").getResultList();
-		em.close();
-		for(Bank b : l){
-			bankChoiceHome.getItems().add(b.getName());
-		}
+		super.initialize(fxmlFileLocation, resources);
 	}
 }
