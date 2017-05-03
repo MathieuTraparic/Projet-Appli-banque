d13177ff2e8bacad1d3793c0062bd4ea6b17600e
DavidRiviere <david_riviere@hotmail.fr>
2017-05-03 12:28:23 +0200

    Export du rib en pdf opérationnel

--- BankEclipse/src/controllers/RIBController.java
+++ BankEclipse/src/controllers/RIBController.java
@@ -5,45 +5,71 @@
  */
 package controllers;
 
+import java.io.File;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
-import com.itextpdf.layout.element.Paragraph;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.stage.DirectoryChooser;
+import javafx.stage.Stage;
 import model.Account;
+import model.Bank;
 import model.Owner;
 import util.CreatePDF;
 import util.IBANHandler;
 
-public class RIBController extends AccountSelector{
+public class RIBController extends AccountSelector {
 
-	
 	@FXML
 	TextField iban;
 	@FXML
 	Button exportRib;
+	@FXML
+	ComboBox<Bank> bankCombo;
+	@FXML
+	ComboBox<Account> accountCombo;
 
-	@FXML public void accountSelected(ActionEvent event) {
-		if(this.accountCombo.getValue()==null){
+	@FXML
+	public void accountSelected(ActionEvent event) {
+		if (this.accountCombo.getValue() == null) {
 			this.iban.setText("");
 			return;
 		}
 		Account a = this.accountCombo.getValue();
-		this.iban.setText(IBANHandler.genrateIBAN(a.getNumber(), a.getAgency().getCounterCode(), a.getAgency().getBank().getCode(), a.getCountryCode().getCode()));
+		this.iban.setText(IBANHandler.genrateIBAN(a.getNumber(), a.getAgency().getCounterCode(),
+				a.getAgency().getBank().getCode(), a.getCountryCode().getCode()));
 		exportRib.setDisable(false);
 	}
-	
+
 	@FXML
-	void handleButtonExportRib(ActionEvent event){
-		CreatePDF ribPDF = new CreatePDF("RIB_2", String.format("%s", iban.getText()));
+	void handleButtonExportRib(ActionEvent event) {
+		DirectoryChooser directoryChooser = new DirectoryChooser();
+        File selectedDirectory = 
+                directoryChooser.showDialog((Stage) exportRib.getScene().getWindow());         
+		List<Owner> listOwner = accountCombo.getValue().getOwners();
+		String owner = null; int ind = 0;
+		for (Owner o : listOwner) {
+			if(listOwner.size()==1){
+				owner = o.getName();
+			}
+			else if(ind==0){
+				owner = o.getName() + " ";
+				ind = ind + 1;
+			}
+			else {
+				owner += o.getName() + " ";
+			}	
+		}
+		CreatePDF ribPDF = new CreatePDF("RIB.pdf", String.format("%s", iban.getText()), bankCombo.getValue().getName(),
+				accountCombo.getValue().getAgency().getName(), owner, selectedDirectory.getAbsolutePath());
 	}
-	
+
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		super.initialize(arg0, arg1);
--- BankEclipse/src/util/CreatePDF.java
+++ BankEclipse/src/util/CreatePDF.java
@@ -1,6 +1,7 @@
 /**
  *@Author: David Riviere 
  *2 mai 2017
+ * Source : http://developers.itextpdf.com/fr
  */
 package util;
 
@@ -15,32 +16,100 @@ import com.itextpdf.kernel.geom.PageSize;
 import com.itextpdf.kernel.pdf.PdfDocument;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.layout.Document;
+import com.itextpdf.layout.element.List;
+import com.itextpdf.layout.element.ListItem;
 import com.itextpdf.layout.element.Paragraph;
 
 public class CreatePDF {
 
-	private String paragraphContent;
-	private String PDFName;
+	private String iban;
+	private String pdfName;
+	private String ownerName;
+	private String bankName;
+	private String agencyName;
+	private String savePath;
 
-	public CreatePDF(String PDFName, String paragraphContent) {
-		this.PDFName = PDFName;
-		this.paragraphContent = paragraphContent;
+	public CreatePDF(String pdfName, String iban, String bankName, String agencyName
+			, String ownerName, String savePath) {
+		checkPdfName(pdfName);
+		checkIban(iban);
+		checkBankName(bankName);
+		checkAgencyName(agencyName);
+		checkOwnerName(ownerName);
+		
+		this.pdfName = pdfName;
+		this.iban = iban;
+		this.bankName = bankName;
+		this.agencyName= agencyName;
+		this.ownerName = ownerName;
+		this.savePath = savePath;
 		
 		PdfWriter writer;
 		try {
-			String name = String.format("C:/Users/Beltharion/Desktop/ProjetBank/Projet-Appli-banque/BankEclipse/%s",this.PDFName);
+			String name = String.format("%s/%s",this.savePath,this.pdfName);
 			FileOutputStream fos = new FileOutputStream(name);
 			writer = new PdfWriter(fos);
 			PdfDocument pdf = new PdfDocument(writer);
 			Document document = new Document(pdf,PageSize.A4);
-			//PdfFont font = PdfFontFactory.createFont(FontConstants.TIMES_ROMAN);
-			addDataToPDF(document, this.paragraphContent);
+			PdfFont font = PdfFontFactory.createFont(FontConstants.TIMES_ROMAN);
+			addDataToPDF(document, this.iban, font, bankName, agencyName,
+					ownerName);
+			document.close();
 		} catch (FileNotFoundException e) {
 		} catch (IOException e){
 		}
 	}
-
-	private static void addDataToPDF(Document document, String paragraphContent) {
-		document.add(new Paragraph(paragraphContent));
+	
+	private static void addDataToPDF(Document document, String iban, PdfFont font,
+			String bankName, String agencyName, String ownerName) {
+		document.add(new Paragraph(String.format("Name of the bank : %s", bankName))).setFont(font);
+		document.add(new Paragraph(String.format("Name of the agency : %s", agencyName))).setFont(font);
+		document.add(new Paragraph(String.format("Owner : %s", ownerName))).setFont(font);
+		document.add(new Paragraph("Iban Number :")).setFont(font);
+		List list = new List()
+			    .setSymbolIndent(12)
+			    .setListSymbol("\u2190")
+			    .setFont(font);
+		list.add(new ListItem(iban));
+		document.add(list);
+	}
+	
+	private void checkPdfName(String pdfName) throws NullPointerException{
+		if(pdfName.equals(null)){
+			throw new NullPointerException("The name of the document can't be null");
+		}
+		int ind=0;
+		for(int i=0;i<pdfName.length();i++){
+			if(pdfName.charAt(i) == '.'){
+				ind =1;
+			}
+		}
+		if(ind==0){
+			throw new IllegalArgumentException("pdfName must have a file extension");
+		}
+	}
+	
+	private void checkIban(String iban) throws NullPointerException{
+		if(iban.equals(null)){
+			throw new NullPointerException("The name of the document can't be null");
+		}
+	}
+	
+	private void checkBankName(String bankName) throws NullPointerException{
+		if(bankName.equals(null)){
+			throw new NullPointerException("The name of the document can't be null");
+		}
+	}
+	
+	private void checkAgencyName(String agencyName) throws NullPointerException{
+		if(agencyName.equals(null)){
+			throw new NullPointerException("The name of the agency can't be null");
+		}
+	}
+	
+	private void checkOwnerName(String ownerName){
+		if(ownerName.equals(null)){
+			throw new NullPointerException("The Owner name can't be null");
+		}
 	}
 }
