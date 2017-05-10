482f48f1447a08ed00e50ac7515b9ce5545cbdb3
MathieuTraparic <mathieu.traparic@gmail.com>
2017-05-10 11:20:34 +0200

    added button disabling to changePswd popup

--- BankEclipse/src/controllers/popups/ChangePasswordController.java
+++ BankEclipse/src/controllers/popups/ChangePasswordController.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
+import javafx.beans.value.ChangeListener;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -23,56 +24,51 @@ import util.PasswordHandler;
  * @author Beltharion
  *
  */
-public class ChangePasswordController extends PopupController<Owner> implements Initializable{
+public class ChangePasswordController extends PopupController<Owner> implements Initializable {
 
 	@FXML
-	private Button changeCancel, changePswd;
-	@FXML 
+	private Button cancelButton, submitButton;
+	@FXML
 	private PasswordField newPswd, confirmPswd;
 	@FXML
-	private Label pswdError, confirmError;
-	private List<Label> errorLabels = null;
-	
+	private Label unequalFieldLabel;
+
 	@Override
 	protected void initializePopupFields(Owner data) {
-		// TODO Auto-generated method stub	
+		// TODO Auto-generated method stub
 	}
-	
+
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		this.errorLabels = new ArrayList<Label>() {
-			{
-				add(pswdError);
-				add(confirmError);
+		ChangeListener<? super String> onChange = (observable, oldValue, newValue) -> {
+			boolean fieldsEquals = this.newPswd.getText().equals(this.confirmPswd.getText());
+			this.submitButton.setDisable(this.newPswd.getText().isEmpty() || this.confirmPswd.getText().isEmpty()
+					|| !fieldsEquals);
+			if(newValue!=null){
+				this.unequalFieldLabel.setVisible(!fieldsEquals);
 			}
 		};
-		errorLabels.forEach(label -> label.setVisible(false));
+		this.newPswd.textProperty().addListener(onChange);
+		this.confirmPswd.textProperty().addListener(onChange);
 	}
-	
-	@FXML 
-	void handleChangeCancel(ActionEvent event){
-		Stage stage = (Stage) changeCancel.getScene().getWindow();
+
+	@FXML
+	void handleChangeCancel(ActionEvent event) {
+		Stage stage = (Stage) cancelButton.getScene().getWindow();
 		stage.close();
 	}
-	
+
 	@FXML
-	void handleChangePswd(ActionEvent event){
-		if(newPswd.getText().isEmpty()){
-			pswdError.setVisible(true);
-		}
-		if(!confirmPswd.getText().equals(newPswd.getText())){
-			confirmError.setVisible(true);
-		}
-		if (errorLabels.stream().allMatch(label -> label.isVisible() == false)) {
-			String password = newPswd.getText();
-			String confirm = confirmPswd.getText();
-			String salt = PasswordHandler.getNewSalt();
-			password = PasswordHandler.hash(salt+confirm);
-			this.getData().setSalt(salt);
-			this.getData().setPswd(password);
-			this.setAsValidated();
-			Stage stage = (Stage) changePswd.getScene().getWindow();
-			stage.close();
-		}
+	void handleChangePswd(ActionEvent event) {
+
+		String password = newPswd.getText();
+		String confirm = confirmPswd.getText();
+		String salt = PasswordHandler.getNewSalt();
+		password = PasswordHandler.hash(salt + confirm);
+		this.getData().setSalt(salt);
+		this.getData().setPswd(password);
+		this.setAsValidated();
+		Stage stage = (Stage) submitButton.getScene().getWindow();
+		stage.close();
 	}
 }
